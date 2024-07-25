8edda1e63e657c09b32394d09cd9565f25b7ed45
ZobaOkafor <judithokafor30@gmail.com>
2024-07-25 03:07:24 +0000

    Create a project using Webpack

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Notifications from "./Notifications";
+import Notifications from './Notifications/Notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,8 +16,3 @@ rootNotifications.render(
     <Notifications />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
