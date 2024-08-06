47f5a5f79011af8f2cf17da8fc45ce6634e1e7c6
ZobaOkafor <judithokafor30@gmail.com>
2024-08-06 09:19:10 -0700

    Aphrodite

--- 0x04-React_inline_styling/task_0/dashboard/src/Login/Login.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/Login/Login.test.js
@@ -1,1 +1,21 @@
-Login.js
+import React from 'react';
+import { shallow } from 'enzyme';
+import Login from './Login';
+
+const wrapper = shallow(<Login />);
+
+it('renders without crashing', () => {
+  shallow(<Login />);
+});
+
+it('renders login', () => {
+  expect(wrapper.find('main.login').exists()).toEqual(true);
+});
+
+it('renders login', () => {
+  expect(wrapper.find('main.login input')).toHaveLength(2);
+});
+
+it('renders login', () => {
+  expect(wrapper.find('main.login label')).toHaveLength(2);
+});
