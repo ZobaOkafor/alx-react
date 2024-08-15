fcc71954376c7abffe3647df5a47d3cf22f7ac0e
ZobaOkafor <judithokafor30@gmail.com>
2024-08-15 11:32:20 -0700

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,18 +1,25 @@
 import { Seq } from 'immutable';
 
-// Capitalizes the first letter of a string
-const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+export default function printBestStudents(object) {
+  const seq = Seq(object);
 
-// Filters and prints students with scores >= 70, with capitalized names
-export default function printBestStudents(grades) {
-  const bestStudents = Seq(grades)
-    .filter(student => student.score >= 70)
-    .map(student => ({
-      ...student,
-      firstName: capitalize(student.firstName),
-      lastName: capitalize(student.lastName),
-    }))
-    .toJS();
+  //   console.log(seq);
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
+  });
 
-  console.log(bestStudents);
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
 }
