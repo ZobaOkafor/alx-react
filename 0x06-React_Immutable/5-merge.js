04e5aff2c5bd27c918e08fa6164683cc6b34e7b3
ZobaOkafor <judithokafor30@gmail.com>
2024-08-15 11:14:25 -0700

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,15 +1,15 @@
 import { Map, List } from 'immutable';
 
-// Concatenates two arrays and returns a List containing their values
 export function concatElements(page1, page2) {
   const list1 = List(page1);
   const list2 = List(page2);
-  return List1.concat(List2);
+
+  return list1.concat(list2);
 }
 
-// Merges two objects and returns a List containing the merged values
 export function mergeElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
+
   return map1.merge(map2);
 }
