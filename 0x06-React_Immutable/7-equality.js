6e3932b27b305734d9dd883d6d63149d2520a024
ZobaOkafor <judithokafor30@gmail.com>
2024-08-15 11:28:05 -0700

    Equality

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,6 +1,6 @@
 import { is, Map } from 'immutable';
 
 // Checks if two Immutable.js Maps are equal
-export function areMapsEqual(map1, map2) {
+export default function areMapsEqual(map1, map2) {
   return is(map1, map2);
 }
