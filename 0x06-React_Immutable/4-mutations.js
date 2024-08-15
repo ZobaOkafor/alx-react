6346b2e20e73f1e60c78270c666ad83a3db11743
ZobaOkafor <judithokafor30@gmail.com>
2024-08-15 10:41:00 -0700

    Chained mutations

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,14 @@
+import { Map } from 'immutable';
+
+// Create an Immutable Map with the given object
+export const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+// Create map2 by modifying specific values in the first map
+export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
